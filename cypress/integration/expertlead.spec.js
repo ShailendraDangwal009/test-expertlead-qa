911fa3160df25f068ab101c6e0a167aaa5165559
ShailendraDangwal009 <48775974+ShailendraDangwal009@users.noreply.github.com>
2019-07-10 00:10:26 +0530

    Update expertlead.spec.js

--- cypress/integration/expertlead.spec.js
+++ cypress/integration/expertlead.spec.js
@@ -1,5 +1,18 @@
 describe('Expertlead broken link test', function(){
+
     it('test will find all broken links', function() {
-        expect(true).to.equal(true)
+    cy.visit('https://www.cypress.io/')
+    cy.get('a').each(($href) => {
+        
+       // var baseURL = "https://www.cypress.io/"
+        //cy.request($href).then((resp) => {
+             
+            
+                cy.log($href)
+             
+       // expect(resp.status).to.eq(200)
+             
+       })
+
     })
 })
